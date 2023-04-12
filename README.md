b93d14bd073822e5cb988bf11682a504d68bc880
Daniil K <daniil@zemzio.com>
2023-04-12 10:51:56 +0300

    main: add utility class for plotting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A system for automatic recognition of emotions by a speech signal based on a neural network model
+# A system for automatic recognition of emotions by a speech signal based on a neural network model
+
+### Install pipreqs: pip install pipreqs (currently we use pipreqs-0.4.11 )
+### Update requirements.txt if needed: pipreqs
+### Install dependencies: pip install -r requirements.txt
---
+++ plotting_utils.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import librosa
+
+
+def plot_spectrogram(signal, name):
+    """Compute power spectrogram with Short-Time Fourier Transform and plot result."""
+    spectrogram = librosa.amplitude_to_db(librosa.stft(signal))
+    plt.figure(figsize=(20, 15))
+    librosa.display.specshow(spectrogram, y_axis="log")
+    plt.colorbar(format="%+2.0f dB")
+    plt.title(f"Log-frequency power spectrogram for {name}")
+    plt.xlabel("Time")
+    plt.show()
