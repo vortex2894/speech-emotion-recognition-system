e305b9bc2f825e967b908419fa8393e7d3b95e61
Daniil K <daniil@zemzio.com>
2023-04-19 07:37:10 +0300

    main: implement method for plotting multiclass confusion matrix

--- plotting_utils.py
+++ plotting_utils.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import librosa
+import pandas as pd
+import seaborn as sns
 
 
 def plot_spectrogram(signal, name):
@@ -11,3 +13,18 @@ def plot_spectrogram(signal, name):
     plt.title(f"Log-frequency power spectrogram for {name}")
     plt.xlabel("Time")
     plt.show()
+
+
+def print_multiclass_confusion_matrix(confusion_matrix, axes, class_label, class_names, font_size=14):
+    df_cm = pd.DataFrame(confusion_matrix,
+                         index=class_names,
+                         columns=class_names)
+    try:
+        heatmap = sns.heatmap(df_cm, annot=True, fmt="d", cbar=False, ax=axes)
+    except ValueError:
+        raise ValueError("Confusion matrix values must be integers.")
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=font_size)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=font_size)
+    axes.set_ylabel('True label')
+    axes.set_xlabel('Predicted label')
+    axes.set_title(class_label)
